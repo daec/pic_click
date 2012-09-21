64e69eca91c8a0943d20fe70af7f41e6a55d37a4
Dae Cristae <dae@Daedalus-Labyrinth.local>
2012-09-20 20:36:16 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicClick::Application.config.secret_token = 'f3059e147ac6b4b83baecb2f7a2af1377e2101630b65b3085b64cc107b703efd53521bc50bdb0ac065d998bea9bab74f19cd85860d8c9f28a496c58a9220f3fb'
