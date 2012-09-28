d8a924b44bada68f828b8ff2617f6d2b8d9d9451
Dae Cristae <dae@Daedalus-Labyrinth.local>
2012-09-27 19:41:02 -0700

    update from last week

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PicClick::Application.routes.draw do
+  resources :images
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ PicClick::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'images#index'
 
   # See how all your routes lay out with "rake routes"
 
